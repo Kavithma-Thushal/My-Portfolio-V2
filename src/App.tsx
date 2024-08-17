3b116af35e6d40ce1a90d8dbbb9b5a3afbd4bb16
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-08-17 11:12:18 +0530

    Refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <div>
       <h1>Hello Vite</h1>
     </div>
   )
 }
-
-export default App
