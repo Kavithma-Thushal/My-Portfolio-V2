0adc6e77051f0f7cabcacaef7a7813ae95185edc
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-08-17 11:49:13 +0530

    tailwind.config.js Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h1 className="text-red-500">Hello Vite</h1>
+      <h1 className="text-neonBlue">Hello Vite</h1>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'darkBlue': '#000a1f',
+        'neonBlue': '#00c7ff',
+      },
+      textColor: {
+        'darkBlue': '#000a1f',
+        'neonBlue': '#00c7ff',
+      }
+    },
   },
   plugins: [],
 }
