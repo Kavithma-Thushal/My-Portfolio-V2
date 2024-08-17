4a7034998367866d3c1668e0da36edb065ee777a
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-08-17 22:11:29 +0530

    Footer Responsived

--- src/views/components/Footer/Footer.tsx
+++ src/views/components/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 export function Footer() {
   return (
     <footer className="bg-darkBlue text-white p-3 w-full fixed bottom-0">
-      <h6 className="text-center">© 2024 All Right Reserved | Designed By{" "}
-        <a href="https://github.com/Kavithma-Thushal?tab=repositories" target={"_blank"}
-          className="font-semibold text-neonBlue transition-colors duration-300">Kavithma Thushal</a>
-      </h6>
+      <div className="container mx-auto text-center">
+        <h6 className="text-sm md:text-base">© 2024 All Right Reserved | Designed By{" "}
+          <a href="https://github.com/Kavithma-Thushal?tab=repositories" target="_blank" rel="noopener noreferrer"
+            className="font-semibold text-neonBlue transition-colors duration-300">Kavithma Thushal</a>
+        </h6>
+      </div>
     </footer>
   );
 }
