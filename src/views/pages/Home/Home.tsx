61795355494ac765898b3843243d17edb76b0fe7
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-08-17 16:06:57 +0530

    Home Refactored 2

--- src/views/pages/Home/Home.tsx
+++ src/views/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ export function Home() {
             <div className="text-center space-y-8">
                 <h1 className="text-6xl font-extrabold animate-fade-in">Welcome to My Portfolio</h1>
                 <p className="text-xl max-w-2xl mx-auto text-gray-200 animate-fade-in-delay">
-                    Explore my work, learn more about my skills,<br /> and discover the projects I've been working on.
+                    Explore my work, learn more about my skills,<br /> and discover the projects I've been working on...
                 </p>
                 <div className="flex justify-center space-x-6">
                     <a href="/My-Portfolio-V2/about" className="bg-neonBlue text-darkBlue px-6 py-3 rounded-lg font-semibold shadow-lg hover:scale-105 transition duration-300">About Me</a>
